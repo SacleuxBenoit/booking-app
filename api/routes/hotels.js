00164a74b181e43eafa19e37464b749defbb5d31
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-03-06 17:23:02 +0100

    :memo: correctif : delete req.params.id for getAll hotels

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -50,10 +50,10 @@ router.get("/:id", async (req,res) => {
 router.get("/", async (req,res) => {
 
     try{
-        const hotels = await Hotel.find(req.params.id)
+        const hotels = await Hotel.find()
         res.status(200).json(hotels);
     }catch(err){
-        res.status(500).json(err)
+        next(err);
     }
 })
 
