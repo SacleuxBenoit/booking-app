c942ee47a415d21700c0322ae5cb48ee6b48f3e8
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-03-09 14:30:01 +0100

    add secret_key + create token

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
 import userRoute from "./routes/users.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
@@ -23,7 +24,7 @@ mongoose.connection.on("disconnected", () => {
 })
 
 // middlewares
-
+app.use(cookieParser())
 app.use(express.json())
 
 app.use("/api/auth", authRoute)
