875a003b2e9a26b10771dc7469806e9222edf8bb
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-03-09 15:29:51 +0100

    add routes && controllers for users (update / delete / get / get all)

---
+++ api/controllers/userController.js
@@ -0,0 +1,37 @@
+import User from "../models/User";
+
+export const updateUser = async (req,res) => {
+    try{
+        await User.findByIdAndUpdate(req.params.id, {$set: req.body},{new:true})
+        res.status(200).json("user has been deleted");
+    }catch(err){
+        next(err)
+    }
+}
+
+export const deleteUser = async (req,res) => {
+    try{
+        const deletedUser = await User.findByIdAndDelete(req.params.id)
+        res.status(200).json(deletedUser);
+    }catch(err){
+        next(err);
+    }
+}
+
+export const getUser = async (req,res) => {
+    try{
+        const user = await User.findById(req.params.id)
+        res.status(200).json(user);
+    }catch(err){
+        next(err)
+    }
+}
+
+export const getUsers = async (req,res) => {
+    try{
+        const users = await User.find()
+        res.status(200).json(users);
+    }catch(err){
+        next(err);
+    }
+}
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,18 @@
 import express from "express";
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
+// UPDATE
+router.put("/:id", updateUser)
+
+// DELETE
+router.delete("/:id", deleteUser)
+
+// GET
+router.get("/:id", getUser)
+
+// GET ALL
+router.get("/", getUsers)
 
 export default router;
