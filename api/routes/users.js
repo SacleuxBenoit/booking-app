19f5267a2a4891eda8f05da8915816d942abbfaf
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-03-10 14:41:36 +0100

    add token verif

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,20 @@
 import express from "express";
 import { deleteUser, getUser, getUsers, updateUser } from "../controllers/userController.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (req,res,next) => {
+    res.send("Hello user, you are logged in")
+})
+
+router.get("/checkuser/:id", verifyUser, (req,res,next) => {
+    res.send("Hello user, you are logged in and you can delete your account")
+})
+
+router.get("/checkadmin/:id", verifyAdmin, (req,res,next) => {
+    res.send("Hello user, you are an admin")
+})
 
 // UPDATE
 router.put("/:id", updateUser)
--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -16,3 +16,28 @@ export const verifyToken = (req,res,next) => {
         next()
     })
 }
+
+export const verifyUser = (req,res,next) => {
+    verifyToken(req,res,next, () =>{
+        if(req.user.id === req.params.id || req.user.isAdmin){
+            next()
+        }else{
+            if(err){
+                return next(createError(403, "You are not authorized"))
+            }
+        }
+    })
+}
+
+export const verifyAdmin = (req,res,next) => {
+    verifyToken(req,res,next, () =>{
+        if(req.user.isAdmin){
+            console.log("admin")
+            next()
+        }else{
+            if(err){
+                return next(createError(403, "You are not authorized"))
+            }
+        }
+    })
+}
